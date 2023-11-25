5a29b9c1c368edae4969a975bff80002dd9af4e5
tsssni <dingyongyu2002@foxmail.com>
2023-11-25 16:46:54 +0800

    add volume control script

---
+++ volume.sh
@@ -0,0 +1,3 @@
+volume="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')"
+volume="$(bc <<< $volume/0.01)"
+dunstify -a "volume" -u normal -t 3000 -i audio-volume-$1-symbolic -h string:x-dunst-stack-tag:volume -h int:value:$volume "" ""
