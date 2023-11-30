c5bc843424e5bdb5249f3031a934bfc3c40654ba
tsssni <dingyongyu2002@foxmail.com>
2023-11-30 23:26:45 +0800

    change icon theme

--- volume.sh
+++ volume.sh
@@ -1,3 +1,3 @@
 volume="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')"
 volume="$(bc <<< $volume/0.01)"
-dunstify -a "volume" -u normal -t 3000 -i media-volume -h string:x-dunst-stack-tag:volume -h int:value:$volume "" ""
+dunstify -a "volume" -u normal -t 3000 -i audio-headphones -h string:x-dunst-stack-tag:volume -h int:value:$volume "" ""
